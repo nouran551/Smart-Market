0e2d1efd70bbc51d012419b63cad6e18c2305d7a
Nouran Waleed <40387105+nouran551@users.noreply.github.com>
2020-10-23 19:25:22 +0200

    Add files via upload

---
+++ Admin Web app/auth.js
@@ -0,0 +1,10 @@
+
+// check if user is logged in
+// if not redirect back to index.html
+/*
+firebase.auth().onAuthStateChanged(function(user){
+    if(!user){
+        window.location.href = "index.html";
+    }
+});
+*/
